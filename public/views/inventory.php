f60fc9712938dd518ebf0c19e3c69a5734c3e900
pawel <pawelzeg@gmail.com>
2021-01-09 19:09:56 +0100

    Added css and html file for inventory view

--- public/views/inventory.php
+++ public/views/inventory.php
@@ -1,10 +1,42 @@
 <!DOCTYPE html>
 </head>
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <script src="https://kit.fontawesome.com/8d0c9bc700.js" crossorigin="anonymous"></script>
     <title>INVENTORY</title>
 </head>
 <body>
-    <div class="container">
-            
+    <div class="base-container">
+        <nav>
+            <img src="public/img/logo_white.svg">
+            <ul class="side_menu">
+                <li>
+                    <a href="#">Manage food</a>
+                    <ul class="side_menu" >
+                        <li><a href="#">My food</a>
+                        <li><a href="#">Add Item</a>
+                        <li><a href="#">Remove Item</a>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Recipes</a>
+                </li>
+            </ul>
+        </nav>
+
+        <main>
+            <header>
+                <div class="search-bar">
+                    <form>
+                        <input placeholder="search">
+                    </form>
+                </div>
+                <div class="account">
+                    
+                </div>
+            </header>
+            <section class="inventory">
+
+            </section>
+        </main>
     </div>
 </body>
