79b2e65482882990d61f18126beb66ada7e43fe3
pawel <pawelzeg@gmail.com>
2021-01-29 18:04:12 +0100

    registration button fix

--- public/views/registered_successfully.php
+++ public/views/registered_successfully.php
@@ -3,7 +3,8 @@ session_start();
 ?>
 <!DOCTYPE html>
 </head>
-<link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/register.css">
 <title>Register</title>
 <script type="text/javascript" src="./public/js/register_validation.js" defer></script>
 </head>
@@ -13,8 +14,7 @@ session_start();
         <div class="modal-content">
             <h2>Your account has been registered!</h2>
 
-            <button>Go to Log in page</button>
-            </form>
+            <button onclick="location.href='./login';">Go to Login page</button>
         </div>
     </div>
 </div>
